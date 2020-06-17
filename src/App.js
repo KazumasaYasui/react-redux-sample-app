ff1c5b29c66ec847ecba8ef59ed59cc73d9bdc24
KazumasaYasui <silver.soul39@gmail.com>
2020-06-18 07:19:35 +0900

    [ADD] state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+// import PropTypes from 'prop-types';
 
 // class App extends Component {
 //     render() {
@@ -41,36 +41,60 @@ import PropTypes from 'prop-types';
 //   )
 // }
 
-const App = () => {
-    const profiles = [
-        { id: 1, name: "Taro", age: 11 },
-        { id: 2, name: "John", age: 23 },
-        { id: 3, name: "Jose", age: 44 },
-        { id: 4, name: "Xi" },
-    ]
-    return (
-        <div>
-            {
-                profiles.map((profile, index) => {
-                    return <User id={profile.id} name={profile.name} age={profile.age} key={index}/>
-                })
-            }
-        </div>
-    )
-}
+// const App = () => {
+//     const profiles = [
+//         { id: 1, name: "Taro", age: 11 },
+//         { id: 2, name: "John", age: 23 },
+//         { id: 3, name: "Jose", age: 44 },
+//         { id: 4, name: "Xi" },
+//     ]
+//     return (
+//         <div>
+//             {
+//                 profiles.map((profile, index) => {
+//                     return <User id={profile.id} name={profile.name} age={profile.age} key={index}/>
+//                 })
+//             }
+//         </div>
+//     )
+// }
+//
+// const User = (props) => {
+//     return <div>Hi, I'm {props.name} and {props.age} years old! (id: {props.id})</div>
+// }
+//
+// User.defaultProps = {
+//     age: 1
+// }
+//
+// User.propTypes = {
+//     id: PropTypes.number.isRequired,
+//     name: PropTypes.string,
+//     age: PropTypes.number
+// }
 
-const User = (props) => {
-    return <div>Hi, I'm {props.name} and {props.age} years old! (id: {props.id})</div>
-}
+const App = () => (<Counter />)
 
-User.defaultProps = {
-    age: 1
-}
+class Counter extends Component {
+    constructor(props) {
+        super(props)
+        this.state = { count: 0 }
+    }
 
-User.propTypes = {
-    id: PropTypes.number.isRequired,
-    name: PropTypes.string,
-    age: PropTypes.number
+    handleButton = (type) => {
+        const num = type === 'up' ? + 1 : - 1
+        this.setState({ count: this.state.count + num })
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <div>count: { this.state.count }</div>
+                <button onClick={(_) => this.handleButton('up')}>+1</button>
+                <button onClick={(_) => this.handleButton('down')}>-1</button>
+            </React.Fragment>
+        )
+    }
 }
 
 export default App;
