3680451b22722a42c32541dfaa0bc15fd4e3fd82
KazumasaYasui <silver.soul39@gmail.com>
2020-06-18 06:06:39 +0900

    [ADD] components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  const first = "Kazu";
-  const last = "Yasu"
-  const dom = (
-      <React.Fragment>
-        <h1 className="foo">Hi, { first + last }!!</h1>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("I'm clicked.")}}/>
-        <label htmlFor="baz">baz</label>
-        <input type="text" onChange={() => {console.log("I'm changed.")}}/>
-      </React.Fragment>
-  );
-  return dom
-}
+// class App extends Component {
+//     render() {
+//         const first = "Kazu";
+//         const last = "Yasu"
+//         const dom = (
+//             <React.Fragment>
+//                 <h1 className="foo">Hi, { first + last }!!</h1>
+//                 <label htmlFor="bar">bar</label>
+//                 <input type="text" onClick={() => {console.log("I'm clicked.")}}/>
+//                 <label htmlFor="baz">baz</label>
+//                 <input type="text" onChange={() => {console.log("I'm changed.")}}/>
+//             </React.Fragment>
+//         );
+//         return dom;
+//     }
+// }
+
+// function App() {
+//   const first = "Kazu";
+//   const last = "Yasu"
+//   const dom = (
+//       <React.Fragment>
+//         <h1 className="foo">Hi, { first + last }!!</h1>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {console.log("I'm clicked.")}}/>
+//         <label htmlFor="baz">baz</label>
+//         <input type="text" onChange={() => {console.log("I'm changed.")}}/>
+//       </React.Fragment>
+//   );
+//   return dom
+// }
 
 // function App() {
 //   return React.createElement(
@@ -22,5 +39,21 @@ function App() {
 //       "Hello, World!!!!!"
 //   )
 // }
+
+const App = () => {
+    return (
+        <div>
+            <Cat />
+            <Cat />
+            <Cat />
+            <Cat />
+            <Cat />
+        </div>
+    )
+}
+
+const Cat = () => {
+    return <div>Meow!</div>
+}
 
 export default App;
